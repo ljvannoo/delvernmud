2354dc9cfc9ba9c7501764c6d7e33bf8cd7e38dc
Unknown <lukas.vannoord@gmail.com>
2018-08-17 14:45:16 -0500

    Add template test

--- db_test.py
+++ db_test.py
@@ -10,6 +10,7 @@ from src.managers.character_manager import CharacterManager
 from src.managers.room_manager import RoomManager
 from src.entities.region import Region
 from src.entities.room import Room, Coordinate
+from src.entities.character import Character, CharacterTemplate
 
 def main():
   props = Properties()
@@ -27,27 +28,37 @@ def main():
   props.load_config()
 
   character_manager = CharacterManager()
-  room_manager = RoomManager()
+  # room_manager = RoomManager()
 
-  character = character_manager.get_character('5b71dc8b2ac5154cbaf5b07f')
-  room = room_manager.get_room('5b71e47c2ac5158358a4f4f5')
+  # character = character_manager.get_character('5b71dc8b2ac5154cbaf5b07f')
+  # room = room_manager.get_room('5b71e47c2ac5158358a4f4f5')
 
-  character.room_id = room.id
-  room.add_character(character)
+  # character.room_id = room.id
+  # room.add_character(character)
 
-  room.save()
-  character.save()
+  # room.save()
+  # character.save()
 
   # region = Region()
   # region.name = 'Starting Region'
   # region.keywords = 'start'
   # region.save()
 
-  # room = Room()
-  # room.name = 'Starting Room'
-  # room.description = 'You find yourself in an empty room.'
-  # room.coordinates = Coordinate(0,0,0)
-  # room.region_id = '5b71e3dc2ac51580c8d6baa6'
-  # room.save()
+  room = Room()
+  room.name = 'Starting Room'
+  room.description = 'You find yourself in an empty room.'
+  room.coordinates = Coordinate(0,0,0)
+  room.region_id = '5b71e3dc2ac51580c8d6baa6'
+  room.save()
+
+  # template = CharacterTemplate()
+  # template.name = 'Generic Human'
+  # template.region_id = '5b71e3dc2ac51580c8d6baa6'
+  # template.save()
+
+  # templates = CharacterTemplate.objects()
+  # print(len(templates))
+  # for template in templates:
+  #   print(template.id)
 
 main()
