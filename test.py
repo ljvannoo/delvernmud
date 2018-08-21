475348da4700037625cc17bfcc5ccdc5af437be4
Unknown <lukas.vannoord@gmail.com>
2018-08-21 15:43:47 -0500

    Prove scoping issue

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-from src.entities.character import Character
 
-character = Character()
-character.name = 'Test'
-character.room_id = '12345'
-print(character.room_id)
-print(character.name)
+class Parent(object):
+  def __init__(self):
+    self._logic_modules = {}
+
+class Child(Parent):
+  def __init__(self):
+    super().__init__()
+
+  def print_modules_id(self):
+    print(id(self._logic_modules))
+
+
+def main():
+  child1 = Child()
+  child1.print_modules_id()
+  child2 = Child()
+  child2.print_modules_id()
+  child1.print_modules_id()
+
+main()
